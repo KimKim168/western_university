3a89fa6c84425687ccc1da32055fbcb7e112bee0
KimKim168 <kimsoreya093@gmail.com>
2025-08-22 15:38:07 +0700

    ddd

--- resources/js/pages/westernUniversityNew/components/demo-image.tsx
+++ resources/js/pages/westernUniversityNew/components/demo-image.tsx
@@ -3,7 +3,7 @@ import { ImageSwiper } from './ui/image-swiper';
 export function DemoImage({ images, imageSrc }) {
     return (
         <div className="mx-auto max-w-screen-2xl px-4 xl:px-16">
-            <ImageSwiper imageSrc={imageSrc} images={images} className='aspect-[16/6]' />
+            <ImageSwiper imageSrc={imageSrc} images={images} />
         </div>
     );
 }
